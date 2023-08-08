8b827427aae5781fe40529c9529a5ce70c5d2b09
VSLuis01 <leoboyluis143@gmail.com>
2023-08-08 23:43:40 +0530

    datatypes

---
+++ 02.datatypes.js
@@ -0,0 +1,54 @@
+/*
+Datatypes in js
+1. Primitive Datatypes
+    - String
+    - Number
+    - Boolean
+    - Undefined
+    - Null
+2. Reference Datatypes
+    - Array
+    - Object
+    - Function
+*/
+
+
+// Primitive Datatypes
+let name = "luis"
+let age = 20
+let isCool = true
+let nullVar = null
+let undef = undefined
+
+
+
+// Reference Datatypes
+let arr = [1,2,3,4,5]
+
+let obj = {
+    name: "luis",
+    age: 20,
+    isCool: true
+}
+
+function func() {
+        
+    }
+
+console.log(typeof arr)
+
+
+// "typeof" is use to check the type of varible
+// console.log(typeof name)
+
+// Note : 
+// 1. null is a object
+// 2. array is a object
+// 3. function is a object
+// 4. object is a object
+// 5. undefined is a undefined
+// 6. string is a string
+// 7. number is a number
+// 8. boolean is a boolean
+// 9. NaN is a number
+// 10. Infinity is a number
