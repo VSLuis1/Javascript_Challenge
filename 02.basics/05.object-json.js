f84cf650169bf194da1c18e04f1e683963d20d27
VSLuis01 <leoboyluis143@gmail.com>
2023-08-14 14:47:01 +0530

    object destructuring and json

---
+++ 02.basics/05.object-json.js
@@ -0,0 +1,25 @@
+// destructuring object
+const shirt = {
+    color : 'blue',
+    size : 'M',
+    price : 200,
+}
+
+const {color, size, price} = shirt; // destructuring object
+console.log(color, size, price);
+
+// JSON - JavaScript Object Notation
+// JSON Example
+
+// Format this data using JSON formatter
+
+
+// {
+//     "name" : "John",
+//     "age" : 30,
+//     "cars" : [
+//         { "name" : "Ford", "models" : ["Fiesta", "Focus", "Mustang"] },
+//         { "name" : "BMW", "models" : ["320", "X3", "X5"] },
+//         { "name" : "Fiat", "models" : ["500", "Panda"] }
+//     ]
+// }
