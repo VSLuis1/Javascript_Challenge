85882a92651592b56de7c57091bdb29d0f7a39c8
VSLuis01 <leoboyluis143@gmail.com>
2023-08-16 00:34:51 +0530

    switch statement

---
+++ 04.control-flow/02.switch.js
@@ -0,0 +1,24 @@
+// Switch statement
+
+// Example
+let month = "march";
+switch (month) {
+    case "january":
+        console.log("January");
+        break;
+    case "february":
+        console.log("February");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+
+    default:
+        console.log("Month not found");
+        break;
+}
+
+
