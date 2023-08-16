2fb6660fe8ca35f957d6cd9297aab018de79ede8
VSLuis01 <leoboyluis143@gmail.com>
2023-08-16 14:53:02 +0530

    for of loop

---
+++ 04.control-flow/05.for-of.js
@@ -0,0 +1,20 @@
+// For of loop - used to iterate over the values in an iterable object (array, string, etc.)
+
+let values = [1,2,3,4,5];
+
+for (const val of values){
+    console.log(val)
+}
+
+// example
+let  fruits = ["Apple, Banana, Orange"];
+
+for (const name of fruits){
+    console.log(fruits)
+}
+
+// for of loop with a string
+let user = "Luis";
+for(const name of user){
+    console.log(name);
+}
