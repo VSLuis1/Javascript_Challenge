6a8e2a649096c7d13cd7c459d75246ebd4efac55
VSLuis01 <leoboyluis143@gmail.com>
2023-08-16 13:10:50 +0530

    do while loop

---
+++ 04.control-flow/04.do..while.js
@@ -0,0 +1,29 @@
+// While loop
+
+// Example 1
+let i = 0;
+while (i <= 5) {
+    console.log(i);
+    i++;
+    }
+
+// Array
+const colors = ['red', 'green', 'blue'];
+let j = 0; // j is the index of the array
+while (j < colors.length) {
+    console.log(colors[j]);
+    j++;
+    }
+
+// do..while loop
+let score = 0;
+do {
+    console.log(score);
+    score++;
+    }  while (score <= 10);
+
+
+
+
+
+
